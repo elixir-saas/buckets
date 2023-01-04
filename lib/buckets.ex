09d6c51d24dae2103c881fcba017516ae1b212a3
Justin Tormey <justin@upsidefinancing.com>
2023-01-04 13:53:46 -0600

    Initial commit

---
+++ lib/buckets.ex
@@ -0,0 +1,18 @@
+defmodule Buckets do
+  @moduledoc """
+  Documentation for `Buckets`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Buckets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/buckets_test.exs
@@ -0,0 +1,8 @@
+defmodule BucketsTest do
+  use ExUnit.Case
+  doctest Buckets
+
+  test "greets the world" do
+    assert Buckets.hello() == :world
+  end
+end
