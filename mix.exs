ec8c42ad6f3f085d0ad1c953ed2cad1a076a075f
Justin Tormey <jrtormey@gmail.com>
2025-05-29 12:45:24 -0500

    v1.0.0-rc.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Buckets.MixProject do
   def project do
     [
       app: :buckets,
-      version: "0.2.0",
+      version: "1.0.0-rc.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
