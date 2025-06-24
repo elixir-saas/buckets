3c939219a8139781b2428f82d6f568397b5333c6
Justin Tormey <jrtormey@gmail.com>
2025-06-24 10:37:19 -0500

    Add test for library version in README

--- test/buckets_test.exs
+++ test/buckets_test.exs
@@ -1,3 +1,20 @@
 defmodule BucketsTest do
   use ExUnit.Case
+
+  test "version in README matches mix.exs" do
+    # Get version from mix.exs
+    mix_version = Buckets.MixProject.project()[:version]
+
+    # Read README content
+    readme_content = File.read!("README.md")
+
+    # Extract version from README installation section
+    version_regex = ~r/{:buckets, "~> ([^"]+)"}/
+
+    assert [_, readme_version] = Regex.run(version_regex, readme_content),
+           "Could not find version in README.md installation section"
+
+    assert readme_version == mix_version,
+           "Version mismatch: mix.exs has '#{mix_version}' but README has '#{readme_version}'"
+  end
 end
